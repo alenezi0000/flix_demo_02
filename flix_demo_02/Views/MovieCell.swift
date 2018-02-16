c8a41e1c873fedac1e1ebb32eb85a6a252e47f99
Aziz Alenezi <azizalenezi@Azizs-MacBook-Air.local>
2018-02-16 12:02:42 -0500

    added tab bar

---
+++ flix_demo_02/Views/MovieCell.swift
@@ -0,0 +1,32 @@
+//
+//  MovieCell.swift
+//  flix_demo_02
+//
+//  Created by Aziz Alenezi on 2/13/18.
+//  Copyright © 2018 Aziz Alenezi. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterimageView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
