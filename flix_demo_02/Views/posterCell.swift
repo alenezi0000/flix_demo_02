fef71967a6d420693e4f47a05e2dc07f00670afc
Aziz Alenezi <azizalenezi@Azizs-MacBook-Air.local>
2018-02-16 17:48:16 -0500

    /Users/azizalenezi/Desktop/flix_demo_02

---
+++ flix_demo_02/Views/posterCell.swift
@@ -0,0 +1,13 @@
+//
+//  posterCell.swift
+//  flix_demo_02
+//
+//  Created by Aziz Alenezi on 2/16/18.
+//  Copyright © 2018 Aziz Alenezi. All rights reserved.
+//
+
+import UIKit
+
+class posterCell: UICollectionViewCell {
+    
+}
